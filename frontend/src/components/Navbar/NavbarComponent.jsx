6f28ce17d758b81d30e5e75224a9c0800790bed2
Adam Vegvari <brokenskijas9@gmail.com>
2024-06-20 19:55:38 +0200

    cart/signup/order finished

--- frontend/src/components/Navbar/NavbarComponent.jsx
+++ frontend/src/components/Navbar/NavbarComponent.jsx
@@ -1,28 +1,56 @@
-import React, { useState } from 'react'
-import { assets } from '../../assets/assets'
-import './NavbarComponent.css'
+import React, { useContext, useState } from "react";
+import { StoreContext } from "../../context/storeContext";
+import { assets } from "../../assets/assets";
+import "./NavbarComponent.css";
+import { Link } from "react-router-dom";
 
-function NavbarComponent() {
-const [menu, setMenu] = useState("home")
-return (
-<div className='navbar'>
-<img src={assets.logo} alt=""  className='logo'/>
-<ul className='navbar-menu'>
-<li onClick={()=> setMenu("home")} className={menu==="home" ? "active" : ""}>Home</li>
-<li onClick={()=> setMenu("menu")} className={menu==="menu" ? "active" : ""}>Menu</li>
-<li onClick={()=> setMenu("mobile-app")} className={menu==="mobile-app" ? "active" : ""}>Mobile-App</li>
-<li onClick={()=> setMenu("contact-us")} className={menu==="contact-us" ? "active" : ""}>Contact Us</li>
-</ul>
-<div className='navbar-right'>
-<img src={assets.search_icon} alt=""/>
-<div className='navbar-search-icon'>
-<img src={assets.basket_icon} alt=""/>
-<div className='dot'></div>
+function NavbarComponent({ setShowLogin }) {
 
+
+  const {getTotalCartAmount} = useContext(StoreContext);
+  const [menu, setMenu] = useState("home");
+  return (
+    <div className="navbar">
+      <Link to="/">
+        <img src={assets.logo} alt="" className="logo" />
+      </Link>
+      <ul className="navbar-menu">
+        <li
+          onClick={() => setMenu("home")}
+          className={menu === "home" ? "active" : ""}
+        >
+          Home
+        </li>
+        <li
+          onClick={() => setMenu("menu")}
+          className={menu === "menu" ? "active" : ""}
+        >
+          Menu
+        </li>
+        <li
+          onClick={() => setMenu("mobile-app")}
+          className={menu === "mobile-app" ? "active" : ""}
+        >
+          Mobile-App
+        </li>
+        <li
+          onClick={() => setMenu("contact-us")}
+          className={menu === "contact-us" ? "active" : ""}
+        >
+          Contact Us
+        </li>
+      </ul>
+      <div className="navbar-right">
+        <img src={assets.search_icon} alt="" />
+        <div className="navbar-search-icon">
+          <Link to="/cart">
+            <img src={assets.basket_icon} alt="" />
+          </Link>
+          <div className={getTotalCartAmount() ===0 ? "" : "dot"}></div>
+        </div>
+        <button onClick={() => setShowLogin(true)}>Sign In</button>
+      </div>
     </div>
-    <button>Sign In</button>
-</div>
-</div>
-)
+  );
 }
-export default NavbarComponent 
+export default NavbarComponent;
--- frontend/src/pages/PlaceOrderPage.jsx
+++ frontend/src/pages/PlaceOrderPage.jsx
@@ -1,8 +1,58 @@
 import React from 'react'
+import { useContext } from 'react'
+import { StoreContext } from '../context/storeContext'
+import './PlaceOrder.css'
+
 
 function PlaceOrderPage() {
+
+  const { getTotalCartAmount } = useContext(StoreContext);
+
   return (
-    <div>PlaceOrderPage</div>
+    <form className="place-order">
+      <div className="place-order-left">
+        <p className="title">Delivery Information</p>
+        <div className="multi-fields">
+          <input type="text" placeholder="First Name" />
+          <input type="text" placeholder="Last Name" />
+        </div>
+        <input type="email" placeholder="Email Adress" />
+        <input type="text" placeholder="Street" />
+        <div className="multi-fields">
+          <input type="text" placeholder="City" />
+          <input type="text" placeholder="State" />
+        </div>
+        <div className="multi-fields">
+          <input type="text" placeholder="Zip Code" />
+          <input type="text" placeholder="Country" />
+        </div>
+        <input type="number" placeholder="Phone Number" />
+      </div>
+
+      <div className="place-order-right">
+      <div className="cart-total">
+          <h2>Cart Total</h2>
+          <div>
+            <div className="cart-total-details">
+              <p>Subtotal:</p>
+              <p>${getTotalCartAmount()}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <p>Delivery Fee:</p>
+              <p>${2}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <b>Total:</b>
+              <b>${getTotalCartAmount() + 2}</b>
+            </div>
+          </div>
+          <button>Proceed To Payment</button>
+        </div>
+
+      </div>
+    </form>
   )
 }
 
